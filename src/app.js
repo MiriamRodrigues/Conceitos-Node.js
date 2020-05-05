b9b9367a04d68419c63219438813f8d9979133f3
Miriam Rodrigues <miriambrodrigues2@gmail.com>
2020-05-05 17:28:52 -0300

    Envio do desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,82 @@ app.use(cors());
 
 const repositories = [];
 
+//Rota GET: Lista todos os repositorios por titulo.
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+
+  const results = title
+  ? repositories.filter(repositorie => repositorie.title.includes(title))
+  : repositories;
+
+  return response.json(results);
 });
 
+//Rota POST: Cria um novo repositorio com likes = 0.
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0};
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
+//Rota PUT: Altera os dados (titulo, url, techs) do repositorio.
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {title, url, techs} = request.body;
+  
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'repositorie not found.'})
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes
+  };
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
 });
 
+//Rota DELETE: Deleta os dados do repositorio de acordo com o id. 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found.'})
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
+//Rota POST: Ao acessar essa rota é adicionado 1 like ao repositorio de determinado id.
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'repositorie not found.'})
+  }
+
+  repositories[repositorieIndex].likes =  repositories[repositorieIndex].likes + 1;
+  const repositorie = { 
+    likes: repositories[repositorieIndex].likes
+  };
+  return response.json(repositorie);
 });
 
 module.exports = app;
